fb89a71875643efcb7e4268f10592a4b34b23004
Yangwook Jeong <yangwookee@gmail.com>
2021-02-04 23:59:28 +0900

    Update splitting functions

--- src/crawler/getCorpInfo.ts
+++ src/crawler/getCorpInfo.ts
@@ -1,5 +1,3 @@
-// TODO: 주가총액 이런것도 추가하기
-
 import { SingleBar } from 'cli-progress'
 import { Page } from 'playwright'
 import * as path from 'path'
@@ -8,24 +6,27 @@ import * as fs from 'fs'
 
 import { csvWriteHeader } from '../utils';
 import { getPlaywright, getProgressBar } from '../middlewares'
+import { ICorpOutline, ICorpMarket, ICorpFinance, ICorpNumber } from '../interfaces'
+import { corpOutlineFields, corpMarketFields, corpFinanceFields} from '../constants'
 
 const username = process.env.DS_USERNAME as string
 const password = process.env.DS_PASSWORD as string
 
-async function getList(page: Page, barl: SingleBar, params: {
-  corpName: string, 
-  filePath: string
-}) {
-  // 검색
-  await page.waitForSelector('.search-bar')
-  await page.$eval('#main-app-bar-search', (el: HTMLInputElement, cpName: string) => el.value = cpName, params.corpName.replace('주식회사', '').replace('(주)', ''))
-  await page.click('.icon-search')
+async function getUserSession (page: Page) {
+  // 로그인 페이지 이동
+  await page.goto('https://www.deepsearch.com/login')
   
-  // 검색결과 슬라이싱
-  // await page.waitForNavigation({
-  //   waitUntil: 'networkidle'
-  // })
-  await page.waitForTimeout(5000)
+  // 로그인
+  await page.$eval('input[placeholder="계정"]', (el: HTMLInputElement, id) => el.value = id, username)
+  await page.$eval('input[placeholder="비밀번호"]', (el: HTMLInputElement, pw) => el.value = pw, password)
+  await page.click('input.button.login')
+
+  // 검색화면 이동
+  await page.waitForSelector('.start-button')
+  await page.click('.start-button')
+}
+
+async function getCorpDetailInfo (page: Page) {
   const data = await page.evaluate(() => {
     const header = Array.from(document.querySelectorAll('#info-list .company-details .info')).map(i => (i.querySelector('.key') as HTMLElement).innerText)
     const values = Array.from(document.querySelectorAll('#info-list .company-details .info')).map((i, idx) => {
@@ -44,12 +45,36 @@ async function getList(page: Page, barl: SingleBar, params: {
     
     return result
   })
+
   console.log({ data })
-  return data
+  return await data
+}
+
+async function getCorpFinancialInfo (page: Page) {
+    
+}
+
+async function getList(page: Page, barl: SingleBar, params: {
+  corpName: string, 
+  filePath: string
+}) {
+  // 검색
+  await page.waitForSelector('.search-bar')
+  await page.$eval('#main-app-bar-search', (el: HTMLInputElement, cpName: string) => el.value = cpName, params.corpName.replace('주식회사', '').replace('(주)', ''))
+  await page.click('.icon-search')
+  
+  // 검색결과 슬라이싱
+  await page.waitForTimeout(5000)
+
+  const details = getCorpDetailInfo(page)
+  const financials = getCorpFinancialInfo(page)
+
+  return { details, financials }
 }
 
 export async function getCorpInfo ({ startDate, endDate }: { startDate: string, endDate: string }) {
-  const filePath = `patent-${startDate}-${endDate}.csv`
+  // 출원인 가져오기 위한 파일 정제 작업
+  const filePath = `examppatent-${startDate}-${endDate}.csv`
 
   const fields = ['businessNumber', 'corpNumber', 'repName', 'estDate', 'address', 'corpName', 'applicantNumber', 'applicantNationality', ]
   const file = fs.createWriteStream(path.join(__dirname, '../../outputs', `corp-${startDate}-${endDate}.csv`), 'utf-8')
@@ -71,33 +96,21 @@ export async function getCorpInfo ({ startDate, endDate }: { startDate: string, 
   await getUserSession(page)
 
   await corporations.reduce(async (prevPromise: any, i: any) => {
+    const params = {
+      filePath,
+      corpName: i.name
+    }
+
     await prevPromise
-    if (i.number.charAt(0) !== '5') {
-      const result = await getList(page, barl, {
-        filePath,
-        corpName: i.name
-      })
+    if (Number(i.number.charAt(0)) !== 5) {
+      const result = await getList(page, barl, params)
       console.log(result)
       
-      file.write(`${result['사업자 등록번호']};${result['법인 등록번호']};${result['대표이사']};${result['설립일자'] ? result['설립일자'] : ''};${result['지번 주소']};${i.name};${i.number};${i.nationality}\n`, err => err && console.log(`> saving file err`))
+      // file.write(`${result.details['사업자 등록번호']};${result.details['법인 등록번호']};${result.details['대표이사']};${result.details['설립일자'] ? result.details['설립일자'] : ''};${result.details['지번 주소']};${i.name};${i.number};${i.nationality}\n`, err => err && console.log(`> saving file err`))
       return result
     }
+
     file.write(`;;;;${i.address};${i.name};${i.number};${i.nationality}\n`, err => err && console.log(`> saving file err`))
-
     return
-  }, <any>Promise.resolve())
-}
-
-async function getUserSession (page: Page) {
-  // 로그인 페이지 이동
-  await page.goto('https://www.deepsearch.com/login')
-  
-  // 로그인
-  await page.$eval('input[placeholder="계정"]', (el: HTMLInputElement, id) => el.value = id, username)
-  await page.$eval('input[placeholder="비밀번호"]', (el: HTMLInputElement, pw) => el.value = pw, password)
-  await page.click('input.button.login')
-
-  // 검색화면 이동
-  await page.waitForSelector('.start-button')
-  await page.click('.start-button')
+  }, Promise.resolve())
 }
