4f6ba3843573bb54e18335b60f9478238dffaf2a
Yangwook Jeong <yangwookee@gmail.com>
2021-02-07 22:30:53 +0900

    Update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,7 +11,46 @@ export const citatingFields: Array<keyof ICitating> = ['applicationNumber', 'nat
 export const citatedFields: Array<keyof ICitated> = ['applicationNumber', 'applicationDate', 'inventionTitle', 'ipcCode']
 export const familyPatentFields: Array<keyof IFamilyPatent> = ['applicationNumber', 'failyNumber', 'nationalityCode', 'nationality', 'failyType']
 
+// Patent files
+export const patentFiles = [{
+  name: 'patent',
+  fields: bibliographicFields
+}, {
+  name: 'ipc',
+  fields: ipcFields
+}, {
+  name: 'cpc',
+  fields: cpcFields
+}, {
+  name: 'applicant',
+  fields: applicantFields
+}, {
+  name: 'inventor',
+  fields: inventorFields
+}, {
+  name: 'claim',
+  fields: claimFields
+}, {
+  name: 'citating',
+  fields: citatingFields
+}, {
+  name: 'citated',
+  fields: citatedFields
+}, {
+  name: 'family',
+  fields: familyPatentFields
+}]
+
 // Corporation
 export const corpOutlineFields: Array<keyof ICorpOutline> = ['applicantNumber', 'businessNumber', 'corpNumber', 'repName', 'estDate', 'address', 'corpName', 'nationality', 'isExtAudit', 'corpScale', 'corpForm', 'indCat', 'isClose', 'isPublic', ]
 export const corpMarketFields: Array<keyof ICorpMarket> = ['corpNumber', 'marketCap']
 export const corpFinanceFields: Array<keyof ICorpFinance> = ['corpNumber', 'totalSales', 'bizProfits', 'crtmNetIncome', 'assets', 'liabilities', 'capital', 'employees', 'roe']
+
+// Corporation files
+export const corpFiles = [{
+  name: 'corp',
+  fields: corpOutlineFields
+}, {
+  name: 'corp-finance',
+  fields: corpFinanceFields
+}]
