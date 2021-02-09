48f121bd2233e20fa285b93f42dc546a5d7e107a
Yangwook Jeong <yangwookee@gmail.com>
2021-02-09 15:30:28 +0900

    Update wait for selector fn for corporation

--- src/crawler/corporation/getDetail.ts
+++ src/crawler/corporation/getDetail.ts
@@ -2,12 +2,13 @@ import { Page } from 'playwright'
 import { fromEntries } from '../../utils'
 
 export async function getCorpDetailInfo (page: Page) {
+  await page.waitForLoadState('domcontentloaded')
   const data = await page.evaluate(() => {
     const result = Array.from(document.querySelectorAll('.company-data.overview .rt-tbody .rt-tr')).map((i, index) => {
       if (index === 0) {
         return [(i.querySelector('.table-data.key') as HTMLElement).innerText, Array.from(i.querySelectorAll('.table-data.value span')).map(j => (j as HTMLElement).innerText)]
       }
-      return (i as HTMLElement).innerText.split('\n')
+      return (i as HTMLElement)?.innerText.split('\n')
     })
   
     return result
@@ -17,8 +18,9 @@ export async function getCorpDetailInfo (page: Page) {
 }
 
 export async function getCorpFinancialInfo (page: Page) {
+  await page.waitForLoadState('domcontentloaded')
   const data = await page.evaluate(() => {
-    const result = Array.from(document.querySelectorAll('.company-data.financial .rt-tbody .rt-tr')).map(i => (i as HTMLElement).innerText.split('\n'))
+    const result = Array.from(document.querySelectorAll('.company-data.financial .rt-tbody .rt-tr')).map(i => (i as HTMLElement)?.innerText.split('\n'))
 
     return result
   })
@@ -27,8 +29,9 @@ export async function getCorpFinancialInfo (page: Page) {
 }
 
 export async function getCorpName (page: Page) {
+  await page.waitForSelector('.ds.header .name')
   const data = await page.evaluate(() => {
-    const result = (document.querySelector('.ds.header .name') as HTMLElement).innerText
+    const result = (document.querySelector('.ds.header .name') as HTMLElement)?.innerText
 
     return result
   })
@@ -37,6 +40,7 @@ export async function getCorpName (page: Page) {
 }
 
 export async function getIsPublic (page: Page) {
+  await page.waitForLoadState('domcontentloaded')
   const data = await page.evaluate(() => {
     const result = (document.querySelector('.ds.header .info') as HTMLElement).nextElementSibling?.innerHTML
 
@@ -47,8 +51,9 @@ export async function getIsPublic (page: Page) {
 } 
 
 export async function getCorpMarketInfo (page: Page) {
+  await page.waitForLoadState('domcontentloaded')
   const data = await page.evaluate(() => {
-    const result = Array.from(document.querySelectorAll('.company-data.market-info .rt-tbody .rt-tr')).map(i => (i as HTMLElement).innerText.split('\n'))
+    const result = Array.from(document.querySelectorAll('.company-data.market-info .rt-tbody .rt-tr')).map(i => (i as HTMLElement)?.innerText.split('\n'))
 
     return result
   })
