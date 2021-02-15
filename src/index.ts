4f9aa2d2969bbe1e8452af7b0c3f512120975c3d
Yangwook Jeong <yangwookee@gmail.com>
2021-02-15 11:14:04 +0900

    Remove importing dotenv from entry file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import moment from 'moment'
-import dotenv from 'dotenv'
 
 import { getCorpInfo, getPatentInfo } from './crawler'
-
-dotenv.config();
 
 (async function () {
   const startDate = '20200101'
   const endDate = moment(new Date).format('YYYYMMDD')
   
-  await getPatentInfo({ startDate, endDate })
-  // await getCorpInfo({ startDate, endDate })
+  // await getPatentInfo({ startDate, endDate })
+  await getCorpInfo({ startDate, endDate })
 })()
