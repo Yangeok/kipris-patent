8c46d13d6705759c2ae98dd07d9a74fe51f8a4a1
Yangwook Jeong <yangwookee@gmail.com>
2021-02-21 11:25:31 +0900

    WIP

--- crawler/src/crawler/finance/index.ts
+++ crawler/src/crawler/finance/index.ts
@@ -1,108 +1,108 @@
-import path from 'path'
-import csv from 'csvtojson'
-import fs from 'fs'
-import dotenv from 'dotenv'
+// import path from 'path'
+// import csv from 'csvtojson'
+// import fs from 'fs'
+// import dotenv from 'dotenv'
 
-import { saveFinanceDetail } from './saveDetail'
+// import { saveFinanceDetail } from './saveDetail'
 
-import { axios, csvWriteHeader, delayPromise, getURL } from '../../utils'
-import { corpFinanceDetailFields } from '../../constants'
-import { getProgressBar } from '../../middlewares'
+// import { axios, csvWriteHeader, delayPromise, getURL } from '../../utils'
+// // import { corpFinanceDetailFields } from '../../constants'
+// import { getProgressBar } from '../../middlewares'
 
-dotenv.config()
+// dotenv.config()
 
-const apiKey = process.env.DATA_PORTAL_API_KEY as string
-const baseUrl = 'http://apis.data.go.kr/1160100/service/GetFinaStatInfoService/'
+// const apiKey = process.env.DATA_PORTAL_API_KEY as string
+// const baseUrl = 'http://apis.data.go.kr/1160100/service/GetFinaStatInfoService/'
 
-async function getBs(arr: any) {
-  const data = arr.map(({ acitNm, crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }: any) => ({ name: acitNm, crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }))
+// async function getBs(arr: any) {
+//   const data = arr.map(({ crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }: any) => ([ crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt ]))
 
-  const result = {
-    currentAssets: data[1] ? data[1] : {},
-    nonCurrentAsset: data[6] ? data[6] : {},
-    currentLiabilities: data[2] ? data[2] : {},
-    totalEquity: data[3] ? data[3] : {},
-    issuedCapital: data[4] ? data[4] : {},
-    totalLiabilities: data[5] ? data[5] : {},
-    nonCurrentLiabilities: data[7] ? data[7] : {},
-    earningSurplus: data[8] ? data[8] : {},
-    totalAssets: data[0] ? data[0] : {},
-  }
+//   const result = {
+//     currentAssets: data[1] ? data[1].join(',') : '',
+//     nonCurrentAsset: data[6] ? data[6].join(',') : '',
+//     currentLiabilities: data[2] ? data[2].join(',') : '',
+//     totalEquity: data[3] ? data[3].join(',') : '',
+//     issuedCapital: data[4] ? data[4].join(',') : '',
+//     totalLiabilities: data[5] ? data[5].join(',') : '',
+//     nonCurrentLiabilities: data[7] ? data[7].join(',') : '',
+//     earningSurplus: data[8] ? data[8].join(',') : '',
+//     totalAssets: data[0] ? data[0].join(',') : '',
+//   }
 
-  return result
-}
+//   return result
+// }
 
-async function getIncoStat(arr: any) {
-  const data = arr.map(({ acitNm, crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }: any) => ({ name: acitNm, crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }))
+// async function getIncoStat(arr: any) {
+//   const data = arr.map(({ crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt }: any) => ([ crtmAcitAmt, pvtrAcitAmt, bpvtrAcitAmt ]))
   
-  const result = {
-    revenue: data[0] ? data[0] : {},
-    operatingIncome: data[1] ? data[1] : {},
-    profitBeforeTax: data[2] ? data[2] : {},
-    profit: data[3] ? data[3] : {},
-  }
+//   const result = {
+//     revenue: data[0] ? data[0].join(',') : '',
+//     operatingIncome: data[1] ? data[1].join(',') : '',
+//     profitBeforeTax: data[2] ? data[2].join(',') : '',
+//     profit: data[3] ? data[3].join(',') : '',
+//   }
 
-  return result
-}
+//   return result
+// }
 
-async function getData({
-  corpNumber,
-  startDate
-}: {
-  corpNumber: string,
-  startDate: string
-}) {
-  const bizYear = String(Number(startDate.substr(0, 4)) -1) 
-  const [
-    { data: json01 },
-    { data: json02 }
-  ] = await Promise.all([
-    delayPromise(await axios.get(`${baseUrl}getBs?serviceKey=${apiKey}&crno=${corpNumber}&bizYear=${bizYear}&resultType=json&numOfRows=10`), 500),
-    delayPromise(await axios.get(`${baseUrl}getIncoStat?serviceKey=${apiKey}&crno=${corpNumber}&bizYear=${bizYear}&resultType=json&numOfRows=10`), 500)
-  ])
+// async function getData({
+//   corpNumber,
+//   startDate
+// }: {
+//   corpNumber: string,
+//   startDate: string
+// }) {
+//   const bizYear = String(Number(startDate.substr(0, 4)) -1) 
+//   const [
+//     { data: json01 },
+//     { data: json02 }
+//   ] = await Promise.all([
+//     delayPromise(await axios.get(`${baseUrl}getBs?serviceKey=${apiKey}&crno=${corpNumber}&bizYear=${bizYear}&resultType=json&numOfRows=10`), 500),
+//     delayPromise(await axios.get(`${baseUrl}getIncoStat?serviceKey=${apiKey}&crno=${corpNumber}&bizYear=${bizYear}&resultType=json&numOfRows=10`), 500)
+//   ])
 
-  if (json01.response.body.items.item.length < 1 || json02.response.body.items.item.length < 1) {
-    return
-  }
-  const bs = await getBs(json01.response.body.items.item)
-  const incoStat = await getIncoStat(json02.response.body.items.item)
+//   // if (json01.response.body.items.item.length < 1 || json02.response.body.items.item.length < 1) {
+//   //   return
+//   // }
+//   const bs = await getBs(json01.response.body.items.item)
+//   const incoStat = await getIncoStat(json02.response.body.items.item)
 
-  return { bs, incoStat }
-}
+//   return { bs, incoStat }
+// }
 
-export async function getFinanceInfo ({
-  startDate, 
-  endDate,
-  outputPath
-}: {
-  startDate: string,
-  endDate: string,
-  outputPath: string
-}) {
-  const filePath = `corp-${startDate}-${endDate}.csv`
+// export async function getFinanceInfo ({
+//   startDate, 
+//   endDate,
+//   outputPath
+// }: {
+//   startDate: string,
+//   endDate: string,
+//   outputPath: string
+// }) {
+//   const filePath = `corp-${startDate}-${endDate}.csv`
   
-  const file = fs.createWriteStream(path.join(__dirname, outputPath, `finance-${startDate}-${endDate}.csv`), 'utf-8')
-  file.write(csvWriteHeader(corpFinanceDetailFields))
+//   const file = fs.createWriteStream(path.join(__dirname, outputPath, `finance-${startDate}-${endDate}.csv`), 'utf-8')
+//   // file.write(csvWriteHeader(corpFinanceDetailFields))
 
-  const corpNumbers: any[] = []
-  await csv({ delimiter: ';' })
-    .fromFile(path.join(__dirname, outputPath, filePath))
-    .then(json => corpNumbers.push(json.map(i => ({
-      corpNumber: i.corpNumber,
-      applicantNumber: i.applicantNumber
-    }))))
+//   const corpNumbers: any[] = []
+//   await csv({ delimiter: ';' })
+//     .fromFile(path.join(__dirname, outputPath, filePath))
+//     .then(json => corpNumbers.push(json.map(i => ({
+//       corpNumber: i.corpNumber,
+//       applicantNumber: i.applicantNumber
+//     }))))
 
-  const barl = getProgressBar()
+//   const barl = getProgressBar()
   
-  await corpNumbers[0].reduce(async (prevPromise: any, i: { corpNumber: string, applicantNumber: string }, idx: number) => {
-    await prevPromise
+//   await corpNumbers[0].reduce(async (prevPromise: any, i: { corpNumber: string, applicantNumber: string }, idx: number) => {
+//     await prevPromise
 
-    barl.start(corpNumbers[0].length, idx + 1)
-    const result = await getData({ corpNumber: i.corpNumber, startDate })
-    if (result !== undefined) {
-      file.write(saveFinanceDetail(i, result), err => err && console.log(`> saving file err`))
-    }
+//     barl.start(corpNumbers[0].length, idx + 1)
+//     const result = await getData({ corpNumber: i.corpNumber, startDate })
+//     if (result !== undefined) {
+//       file.write(saveFinanceDetail(i, result), err => err && console.log(`> saving file err`))
+//     }
 
-    return
-  }, Promise.resolve())
-}
+//     return
+//   }, Promise.resolve())
+// }
--- crawler/src/crawler/finance/saveDetail.ts
+++ crawler/src/crawler/finance/saveDetail.ts
@@ -1,13 +1,32 @@
-export function saveFinanceDetail ({
-  corpNumber, applicantNumber
-}: {
-  corpNumber: string
-  applicantNumber: string
-}, {
-  bs, incoStat
-}: {
-  bs: any
-  incoStat: any
-}) {
-  return `${corpNumber};${applicantNumber};${JSON.stringify(bs)};${JSON.stringify(incoStat)}\n`
-}
+// export function saveFinanceDetail ({
+//   corpNumber, applicantNumber
+// }: {
+//   corpNumber: string
+//   applicantNumber: string
+// }, {
+//   bs, incoStat
+// }: {
+//   bs: any
+//   incoStat: any
+// }) {
+//   `
+//   ${corpNumber};
+//   ${applicantNumber};
+//   ${bs.currentAssets};
+//   ${bs.nonCurrentAsset};
+//   ${bs.currentLiabilities};
+//   ${bs.totalEquity};
+//   ${bs.issuedCapital};
+//   ${bs.totalLiabilities};
+//   ${bs.nonCurrentLiabilities};
+//   ${bs.earningSurplus};
+//   ${bs.totalAssets};
+//   ${incoStat.revenue};
+//   ${incoStat.operatingIncome};
+//   ${incoStat.profitBeforeTax};
+//   ${incoStat.profit};
+//   ${JSON.stringify(incoStat)}
+//   \n`
+
+//   return `${corpNumber};${applicantNumber};${bs.currentAssets};${bs.nonCurrentAsset};${bs.currentLiabilities};${bs.totalEquity};${bs.issuedCapital};${bs.totalLiabilities};${bs.nonCurrentLiabilities};${bs.earningSurplus};${bs.totalAssets};${incoStat.revenue};${incoStat.operatingIncome};${incoStat.profitBeforeTax};${incoStat.profit};\n`
+// }
