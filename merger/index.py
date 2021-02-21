61f8b92a40b492afd37a1d95ee85098a5ae33f41
Yangwook Jeong <yangwookee@gmail.com>
2021-02-21 15:07:18 +0900

    Update type integration to export to excel

--- merger/index.py
+++ merger/index.py
@@ -15,19 +15,31 @@ input_corp_file = f'./corp-{start_date}-{end_date}.csv'
 output_xl_file = f'./{start_date}-{end_date}.xlsx'
 output_csv_file = f'./{start_date}-{end_date}.csv'
 
-df1 = pd.read_csv(input_patent_file, delimiter=';', dtype={'applicantNumber': str})
-df2 = pd.read_csv(input_corp_file, delimiter=';', dtype={'corpNumber': str, 'applicantNumber': str, 'businessNumber': str})
+df1_dtype = {
+  'applicantNumber': str, 
+  'publishNumber': str, 
+  'intlApplNumber': str, 
+  'intlPublishNumber': str
+}
+df2_dtype = {
+  'corpNumber': str, 
+  'applicantNumber': str, 
+  'businessNumber': str
+}
+
+df1 = pd.read_csv(input_patent_file, delimiter=';', dtype=df1_dtype)
+df2 = pd.read_csv(input_corp_file, delimiter=';', dtype=df2_dtype)
 
 # 파일 병합
 df = pd.merge(df1, df2, how='outer', on='applicantNumber')
 
 # 타입 통일
-# df[[col for col in df.columns if df[col].dtypes == object]] = df[[
-#     col for col in df.columns if df[col].dtypes == object]].astype('string')
-# df[[col for col in df.columns if df[col].dtypes == 'int64']] = df[[
-#     col for col in df.columns if df[col].dtypes == 'int64']].astype('string')
-# df[[col for col in df.columns if df[col].dtypes == 'float64']] = df[[
-#     col for col in df.columns if df[col].dtypes == 'float64']].astype('string')
+df[[col for col in df.columns if df[col].dtypes == object]] = df[[
+    col for col in df.columns if df[col].dtypes == object]].astype('string')
+df[[col for col in df.columns if df[col].dtypes == 'int64']] = df[[
+    col for col in df.columns if df[col].dtypes == 'int64']].astype('string')
+df[[col for col in df.columns if df[col].dtypes == 'float64']] = df[[
+    col for col in df.columns if df[col].dtypes == 'float64']].astype('string')
 
 # 파일 저장
 df.to_csv(output_csv_file, index=False, header=True, sep=';')
